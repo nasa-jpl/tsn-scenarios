ac7a68d64ef956c79e9009184cde03a1602538b4
Mike Thielman <thielman@jpl.nasa.gov>
2025-08-04 11:47:34 -0700

    docs: Added a lot more comments

--- scenarios/ixnetwork/802.1CB-stream-ident/4.py
+++ scenarios/ixnetwork/802.1CB-stream-ident/4.py
@@ -42,10 +42,10 @@ load_dotenv()
 username = os.getenv('username')
 password = os.getenv('password')
 
+# TODO: Figure out a better / external way to provide deployment-specific information about the Keysight and the port mapping so the demo can be run with different TSN switches wired to different ports
+
 # Provide a name for the keysight session
 scenarioName = 'stream_id-4-ip_matching'
-
-# TODO: Figure out a better / external way to provide deployment-specific information about the Keysight and the port mapping so the demo can be run with different TSN switches wired to different ports
 
 # Our API server and chassis are same device
 apiServerIp = '192.168.1.21'
@@ -131,17 +131,26 @@ try:
     # it just seems to alternate left-right-left-right.
 
     # Setup EP1
+    # To send or receive data on a port on Keysight, you need a 'topology' which contains 1 or more 'device groups' 
+    # which contains 1 or more 'protocol stacks'.  The protocol stacks for TSN scenarios always start with Ethernet.
+    # If using raw ethernet (with or without VLANs), that is all that is needed.  If using IPv4, that is added on top of the Ethernet.
     ixNetwork.info('Creating Topology Group 1')
     ep1_topology = ixNetwork.Topology.add(Name='EP1', Ports=vport['Port_EP1'])
     ep1_dg1 = ep1_topology.DeviceGroup.add(Name='EP1.DG1', Multiplier='1')
     ep1_eth1 = ep1_dg1.Ethernet.add(Name='EP1.DG1.Eth1')
+
+    # Note that there are are certain uses for some of the upper bits of MAC address.  Recommend always using 00 for the most significant 8 bits.
     ep1_eth1.Mac.Single(value='00:11:01:00:00:01')
 
-    #   This scenario doesn't use VLANs but leaving this here so somebody starting with this, making a scenario that does use VLANs, knows where to look in the API
-    #   ep1_ethernet.EnableVlans.Single(True)
-    #   ixNetwork.info('Configuring vlanID')
-    #   vlanObj = ep1_ethernet.Vlan.find()[0].VlanId.Increment(start_value=2, step_value=0)
+    # This scenario doesn't use VLANs but leaving this here so somebody starting with this, 
+    # making a scenario that does use VLANs, knows where to look in the API
 
+    # ep1_eth1.EnableVlans.Single(True)
+    # ep1_eth1_vlan = ep1_eth1.Vlan.find()[0].VlanId.SingleValue(2)
+
+    # For IPv4, specify an IP address on the same subnet as the Gateway (by setting prefix to 24, and using the same first 3 numbers x.y.z.*)
+    # The Gateway does not need to exist, but keysight will not generate traffic correctly otherwise
+    # Resolve Gateway needs to be deselected so the keysight does not try to actually access the gateway
     ixNetwork.info('Configuring EP1 IP1')
     ip1 = ep1_eth1.Ipv4.add(Name='EP1.IP1')
     ip1.Address.Single(value='100.1.0.1')
@@ -150,6 +159,8 @@ try:
     ip1.ResolveGateway.Single(False)
 
     # Setup EP3
+    # For this scenario there are 2 separate stacks with separate IP addresses
+
     ixNetwork.info('Creating Topology Group 3')
     ep3_topology = ixNetwork.Topology.add(Name='EP3', Ports=vport['Port_EP3'])
     ep3_dg1 = ep3_topology.DeviceGroup.add(Name='EP3.DG1', Multiplier='1')
@@ -189,7 +200,7 @@ try:
     ip2.ResolveGateway.Single(False)
  
 
-    # Configure UDP Traffic items
+    # Configure UDP Traffic items.  Comments further down explain some of this.
     trafficTypeList = ['ipv4', 'ipv4', 'ipv4', 'ipv4', 'ipv4', 'ipv4', 'ipv4', 'ipv4', 'ipv4', 'ipv4', 'ethernetVlan']
     sourceList = [ep1_topology, ip1, ip1, ip1, ip2, ip2, ip2, ip2, ip2, ip2, ep1_eth1]
     destList = [ip3, ip3, ip4, ip4, ip3, ip3, ip4, ip4, ip4, ip3, ep3_eth1]
@@ -199,31 +210,54 @@ try:
     
     ixNetwork.info('Create Traffic Items')
     trafficItem = []
-    for i in range(len(sourceList)):
+    for i in range(len(sourceList)-1):
+        # Create a traffic item.  This scenario, all traffic is uni-directional.  
+        # Need to specify the type so that the appropriate packet headers are applied.
         trafficItem.append(ixNetwork.Traffic.TrafficItem.add(Name='Traffic Item '+str(i), BiDirectional=False, TrafficType=trafficTypeList[i]))
+
+        # Add the source and destination.  Note that a variety of types are supported here - 
+        # you can specify a topology, or a stack like ethernet or IPv4.
+
+        # If you specify something that has multiple stacks, then you get the frames split 
+        # between them which is typically not what we want.
+
+        # So for example if a topology ep3_topology has 2 IPv4 stacks, you need to provide 
+        # the specific IPv4 stack you want to use.
         trafficItem[i].EndpointSet.add(Sources=sourceList[i], Destinations=destList[i])
 
         # Note: A Traffic Item could have multiple EndpointSets (Flow groups).  Therefore, ConfigElement is a list.
         configElement = trafficItem[i].ConfigElement.find()[0]
 
+        # If this traffic item is UDP, add the UDP packet header with appropriate destination port.
+        # Our scenario doesn't care about the source port.
         if(udpList[i]):
             udpFieldObj = createPacketHeader(trafficItem[i], packetHeaderToAdd='UDP', appendToStack='IPv4')
             udpDstField = udpFieldObj.find(DisplayName='UDP-Dest-Port')
             udpDstField.Auto = False
             udpDstField.SingleValue = destPort[i]
 
+        # Configure for a particular bit rate.  By fixing frame size at 128 bytes, Keysight will determine 
+        # the correct frame rate to use to achieve the specified bit rate.
         configElement.FrameRate.update(Type='bitsPerSecond', BitRateUnitsType='kbitsPerSec', Rate=txRate[i])
         configElement.FrameSize.FixedSize = 128
+
+        # This adds Traffic Item to the Statistics Tracking field.  
+        # Without this, keysight will not track frame drops, latencies, etc.
         trafficItem[i].Tracking.find()[0].TrackBy = ['trackingenabled0']
 
+        # This generates the frames based on the previous configuration.
         trafficItem[i].Generate()
 
 
-#    ixNetwork.Traffic.TrafficItem.Generate(trafficItem)
+    # ixNetwork.Traffic.TrafficItem.Generate(trafficItem)
     ixNetwork.Traffic.Apply()
-#    ixNetwork.StartAllProtocols(Arg1='sync')
+    # ixNetwork.StartAllProtocols(Arg1='sync')
     time.sleep(1)
-#    ixNetwork.Traffic.StartStatelessTrafficBlocking()
+
+    # TODO: Something is not working.  When executing this, it only starts the non-IPv4 traffic item.  
+    # But I can get it to work from the GUI so it seems the end points and traffic items are all created correctly.
+
+    # ixNetwork.Traffic.StartStatelessTrafficBlocking()
     ixNetwork.Traffic.Start()
     time.sleep(1)
 
@@ -245,10 +279,10 @@ try:
     time.sleep(1)
     ixNetwork.Traffic.Stop()
     time.sleep(1)
-    ixNetwork.Traffic.StopStatelessTrafficBlocking()
-    time.sleep(1)
-    ixNetwork.StopAllProtocols(Arg1='sync')
-    time.sleep(1)
+    # ixNetwork.Traffic.StopStatelessTrafficBlocking()
+    # time.sleep(1)
+    # ixNetwork.StopAllProtocols(Arg1='sync')
+    # time.sleep(1)
 
     if debugMode == False:
         for vport in ixNetwork.Vport.find():
