5a2a8d6a2bdcda9fc341fd1b7d7c25ec143c2269
Rob Donnelly <Robert.Donnelly@jpl.nasa.gov>
2025-11-04 18:28:02 -0800

    Port stream gate scenario 1 to pytest
    Switch Configuration
    ====================
    
    Stream identification was changed from src mac to dst mac to fix issue
    with 802.1AS traffic getting dropped.  Previous ad hoc script
    implementation must have used a unique MAC address for 802.1AS traffic
    vs data traffic so that only data traffic was dropped.  Now that pytest
    implementation uses same MAC for both 802.1AS traffic and data traffic,
    need a more discerning way to identify the data traffic.
    
    New Helpers
    ===========
    
    This commit adds new helpers to be able to make assertions on stats that
    are only available when traffic is in-progress (e.g., tx/rx rates).
    Previous to this, we could only query stats that remained after traffic
    was stopped.

--- scenarios/ixnetwork/ixnetwork_restpy_helpers.py
+++ scenarios/ixnetwork/ixnetwork_restpy_helpers.py
@@ -46,6 +46,121 @@ class StatsViewSnapshot:
         return self._snapshot[i]
 
 
+class AssertStats:
+    def __init__(self, ixn, view_caption: str, timeout: int = 60):
+        self._caption = view_caption
+        self._entity = self._caption.split()[0]
+        self._view = ixn.Statistics.View.find(Caption=f"^{view_caption}$")
+
+        logger.info(f"Waiting for {self._caption}")
+        for _ in range(timeout):
+            is_ready = self._view.Data.IsReady
+            if is_ready:
+                break
+            time.sleep(1)
+
+        if not is_ready:
+            raise RuntimeError(f"{self._caption} not ready after {timeout} seconds")
+
+    def assert_equal_eventually(
+        self, index: int, stat: str, value: str | int, timeout: int = 10
+    ):
+        expected = value
+
+        for _ in range(timeout):
+            values = self._view.GetColumnValues(stat)
+            actual = self._cast_as(values[index], expected)
+            if actual == expected:
+                break
+            time.sleep(1)
+
+        logger.info(
+            f"Assert {self._entity} {index} {stat} equals {expected} (actual={actual})"
+        )
+        assert actual == expected
+
+    def assert_approx_eventually(
+        self,
+        index: int,
+        stat: str,
+        value: int,
+        tolerance: float = 1e-3,
+        timeout: int = 10,
+    ):
+        expected = pytest.approx(float(value), rel=tolerance)
+
+        for _ in range(timeout):
+            values = self._view.GetColumnValues(stat)
+            actual = self._cast_as(values[index], expected)
+            if actual == expected:
+                break
+            time.sleep(1)
+
+        logger.info(
+            f"AssertStats('{self._caption}').assert_approx_eventually(index={index} stat='{stat}' actual={actual} expected={expected})"
+        )
+        assert actual == expected
+
+    def _cast_as(self, value, reference):
+        match reference:
+            case int():
+                return int(float(value))
+            case _:
+                raise RuntimeError()
+
+
+class RunTraffic:
+    def __init__(self, ixn):
+        self.ixn = ixn
+
+    def __enter__(self):
+        logger.info("Starting traffic")
+
+        # Generate and apply
+        self.ixn.Traffic.TrafficItem.find().Generate()
+        self.ixn.Traffic.Apply()
+        self.ixn.ClearStats()
+
+        # Start traffic
+        self.ixn.Globals.Testworkflow.Start(False)
+        _wait_for_testworkflow_operation_complete(self.ixn)
+        logger.info("Started traffic")
+
+    def __exit__(self, type, value, traceback):
+        logger.info("Stopping traffic")
+
+        # Stop traffic
+        self.ixn.Globals.Testworkflow.Stop()
+        _wait_for_testworkflow_operation_complete(self.ixn)
+
+
+def _wait_for_testworkflow_operation_complete(ixn, timeout: int = 30):
+    """
+    If initial state is idle, wait for another.
+    If current status is idle, and previous state is not idle, then break.
+    """
+
+    previous_state = None
+
+    for _ in range(timeout):
+        current_state = ixn.Globals.Testworkflow.CurrentState
+        match (previous_state, current_state):
+            case (None, "kIdle"):
+                pass
+            case (_, "kIdle"):
+                break
+            case _:
+                pass
+
+        previous_state = current_state
+        time.sleep(1)
+
+    if current_state != "kIdle":
+        raise RuntimeError(
+            f"State not idle after {timeout} seconds.  State is {current_state}."
+        )
+
+
 def run_traffic_blocking(ixn, timeout_secs: int = 10):
     """
     Runs traffic until completion (e.g., fixed frame count transmitted) or
